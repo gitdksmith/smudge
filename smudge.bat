bbfb51464e3eb49f46f0c838b5af0e1665de7217
Joel Henning <joel_henning@utexas.edu>
2017-04-24 20:34:23 -0500

    testing email notifications

--- smudge.bat
+++ smudge.bat
@@ -1,3 +1,3 @@
 ECHO OFF
-ECHO C:\Python27\pythonw C:\Python27\Lib\idlelib\idle.py -r C:\Users\d\Documents\projects\smudge\smudge.py
+ECHO C:\Python27\pythonw C:\Python27\Lib\idlelib\idle.py -r C:\Users\jsh93_000\Documents\GitHub\smudge\smudge.py
 C:\Python27\pythonw C:\Python27\Lib\idlelib\idle.py -r C:\Users\d\Documents\projects\smudge\smudge.py
